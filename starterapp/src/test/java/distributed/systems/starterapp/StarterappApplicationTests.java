c5cabc44e933ba8d8dd9b69c55bd0a8deb1a4256
StraysWonderland <cagri.c.tasci@outlook.de>
2026-03-27 12:24:20 +0100

    initial app setup

---
+++ starterapp/src/test/java/distributed/systems/starterapp/StarterappApplicationTests.java
@@ -0,0 +1,13 @@
+package distributed.systems.starterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
